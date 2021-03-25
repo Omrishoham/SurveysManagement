d7585e60e508ec9380da189026fe18f0aeae50c0
Omrishoham <omri.s13@gmail.com>
2021-03-25 10:46:08 +0200

    fix config upload

--- server/config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    googleClientID:'745081117498-7oge31rpkpms9avcjedbktj4fsd1fk5t.apps.googleusercontent.com',
-    googleClientSecret:'ozTHUiaE9FhesLkTN_de4jOp',
-    mongoURI:'mongodb+srv://omris13:28631Os@cluster0.kdfxm.mongodb.net/surveys-management?retryWrites=true&w=majority',
-    cookieKey:'adasdasdasdasdafdfxcvxcvxcv'
-}
-
